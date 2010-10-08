905aa803636b5bb4189febfb145897bdc85d746b
Cory Giles <cory.b.giles@gmail.com>
2010-10-07 19:50:11 -0500

    parallel C#/VB versions

--- src/GOGraph.vb
+++ src/GOGraph.vb
@@ -1,6 +1,9 @@
 ﻿Imports System.IO
 Imports QuickGraph
 Imports QuickGraph.Algorithms
+Imports QuickGraph.Algorithms.Search
+Imports QuickGraph.Algorithms.Observers
+Imports QuickGraph.Algorithms.AlgorithmExtensions
 
 <ComClass(GOGraph.ClassId, GOGraph.InterfaceId, GOGraph.EventsId)> _
 Public Class GOGraph
@@ -21,13 +24,15 @@ Public Class GOGraph
     Public Sub New()
         MyBase.New()
     End Sub
-    Dim graphs As New Dictionary(Of String, UndirectedGraph(Of Int32, UndirectedEdge(Of Int32)))
+    Dim graphs As New Dictionary(Of String, AdjacencyGraph(Of Int32, Edge(Of Int32)))
+    Dim undirectedGraphs As New Dictionary(Of String, UndirectedGraph(Of Int32, UndirectedEdge(Of Int32)))
 
     Public Sub InitializeGraph(ByVal goPath As String)
-        Dim graph As UndirectedGraph(Of Int32, UndirectedEdge(Of Int32))
+        Dim graph As AdjacencyGraph(Of Int32, Edge(Of Int32))
         Dim line, goNamespace As String
         Dim currentTerm, parentTerm As Int32
 
+        'Initialize directed graphs
         Using reader As StreamReader = New StreamReader(goPath)
             While Not reader.EndOfStream
                 line = reader.ReadLine
@@ -39,20 +44,62 @@ Public Class GOGraph
                     goNamespace = line.Substring(11)
                 ElseIf line.StartsWith("is_a:") Then
                     If Not graphs.ContainsKey(goNamespace) Then
-                        graphs.Add(goNamespace, New UndirectedGraph(Of Int32, UndirectedEdge(Of Int32)))
+                        graphs.Add(goNamespace, New AdjacencyGraph(Of Int32, Edge(Of Int32)))
                     End If
                     graph = graphs.Item(goNamespace)
 
                     graph.AddVertex(currentTerm)
                     parentTerm = Int32.Parse(line.Substring(10, 7))
                     graph.AddVertex(parentTerm)
-                    graph.AddEdge(New UndirectedEdge(Of Int32)(currentTerm, parentTerm))
+                    graph.AddEdge(New Edge(Of Int32)(currentTerm, parentTerm))
                 End If
 
             End While
         End Using
 
+        'Initialize undirected graphs
+        For Each ns In graphs.Keys
+            Dim g = graphs.Item(ns)
+            Dim newGraph As New UndirectedGraph(Of Int32, UndirectedEdge(Of Int32))
+            For Each e In g.Edges
+                newGraph.AddVertex(e.Source)
+                newGraph.AddVertex(e.Target)
+                newGraph.AddEdge(New UndirectedEdge(Of Int32)(e.Source, e.Target))
+            Next
+            undirectedGraphs.Add(ns, newGraph)
+        Next
     End Sub
+
+    Public Function FindAllAncestors(ByVal node As Int32)
+        Dim result = FindAncestorSet(node)
+        Dim outArr(result.Count - 1) As Int32
+        result.CopyTo(outArr)
+        Return outArr
+    End Function
+
+    Public Function IsAncestor(ByVal parent As Int32, ByVal child As Int32)
+        Return FindAncestorSet(child).Contains(parent)
+    End Function
+
+    Private Function FindAncestorSet(ByVal node As Int32) As HashSet(Of Int32)
+        Dim graph As AdjacencyGraph(Of Int32, Edge(Of Int32))
+        For Each g In graphs.Values
+            If g.ContainsVertex(node) Then graph = g
+        Next
+        If graph Is Nothing Then Return New HashSet(Of Int32)
+        Return FindAncestorSet(graph, node)
+    End Function
+
+    Private Function FindAncestorSet(ByRef graph As AdjacencyGraph(Of Int32, Edge(Of Int32)), ByVal node As Int32) As HashSet(Of Int32)
+        Dim result As New HashSet(Of Int32)
+        For Each outEdge In graph.OutEdges(node)
+            result.Add(outEdge.Target)
+            For Each i In FindAncestorSet(graph, outEdge.Target)
+                result.Add(i)
+            Next
+        Next
+        Return result
+    End Function
 
     Dim costFn As Func(Of UndirectedEdge(Of Int32), Double) = Function() 1
     Public Function ShortestPath(ByVal startNode As Int32, ByVal endNode As Int32) As Int32()
@@ -60,7 +107,7 @@ Public Class GOGraph
 
         'Choose appropriate graph (or return nothing if terms aren't in same namespace)
         Dim graph As UndirectedGraph(Of Int32, UndirectedEdge(Of Int32))
-        For Each g In graphs.Values
+        For Each g In undirectedGraphs.Values
             If g.ContainsVertex(startNode) And g.ContainsVertex(endNode) Then
                 graph = g
             End If
@@ -96,9 +143,15 @@ Module Test
     End Sub
 
     Sub main()
-        MsgBox(Guid.NewGuid().ToString)
         graph.InitializeGraph("C:/data/gene_ontology_ext.obo")
         PrintPath(8285, 8150)
         PrintPath(19568, 8150)
+        For Each i In graph.FindAllAncestors(8285)
+            Console.WriteLine(i)
+        Next
+        Console.WriteLine("-----")
+        Console.WriteLine(graph.IsAncestor(8150, 8285))
+        Console.WriteLine(graph.IsAncestor(8151, 8285))
+        Console.ReadKey()
     End Sub
 End Module
